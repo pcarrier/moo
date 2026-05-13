2cabad7c3d3b0abb5e40a2910aa103c46a1dc337
Pierre Carrier <pc@rrier.fr>
2026-05-14 00:24:29 +0200

    v0.2.8

--- flake.nix
+++ flake.nix
@@ -164,7 +164,7 @@
           commonArgs = {
             inherit src;
             pname = "moo";
-            version = "0.2.7";
+            version = "0.2.8";
             strictDeps = true;
             doCheck = false;
 
